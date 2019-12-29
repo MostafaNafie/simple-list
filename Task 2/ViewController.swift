54efca8ffa400a7b2ec52b22b9c055d90a987c1c
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2019-12-29 15:16:05 +0200

    Initial Commit

---
+++ Task 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task 2
+//
+//  Created by Mustafa on 29/12/19.
+//  Copyright © 2019 Mustafa Nafie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
